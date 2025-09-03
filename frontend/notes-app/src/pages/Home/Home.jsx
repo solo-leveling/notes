5750537e37a70468487d80dd1c55f619f0b46bc3
Hein Htet <littleshadow.mdy@gmail.com>
2025-09-04 00:43:04 +0900

    finished Note

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -37,9 +37,15 @@ const Home = () => {
                     }
                 }}
                 contentLabel=""
-                className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-auto"
+                className="w-[40%] max-h-[95%] bg-white rounded-md mx-auto mt-12 p-5 overflow-y-auto"
             >
-            <AddEditNote />
+                
+                <AddEditNote
+                    type={openAddEditModal.type}
+                    noteData={openAddEditModal.data}
+                    onClose={() => {
+                    setAddEditModal({isShown:false, type:"add", data:null })
+            }}/>
             </Modal>
 
         </>
