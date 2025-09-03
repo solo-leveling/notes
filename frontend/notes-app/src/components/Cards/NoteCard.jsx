3217da5b9fcd87522e424cb2a4323f74c632fb19
Hein Htet <littleshadow.mdy@gmail.com>
2025-09-03 11:53:11 +0900

    Added Note Card

---
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { MdCreate, MdDelete, MdOutlinePushPin } from 'react-icons/md'
+
+const NoteCard = ({title, date, content, tags, isPinned, onEdit, onDelete, OnPinNote}) => {
+    return (
+        <div className='border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out'>
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h6 className='text-sm font-medium'>{ title }</h6>
+                    <span className='text-xs text-slate-500'>{ date }</span>
+
+                </div>
+                <MdOutlinePushPin className={`icon-btn ${isPinned ? 'text-primary' : 'text-slate-400'}`} onClick={OnPinNote} />
+            </div>
+            <p className='text-xs text-slate-600 mt-2'>{content?.slice(0, 60)}</p>
+            
+            <div className='flex items-center justify-between mt-2'>
+                <div className='text-xs text-slate-500'> { tags }</div>
+                <div className='flex items-center gap-2'>
+                    <MdCreate className='btn-icon hover:text-green-500' onClick={onEdit}/>
+                    <MdDelete className='btn-icon hover:text-red-600' onClick={onDelete}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NoteCard
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
 
 const Home = () => {
     return (
-        <div>
-            <Navbar/>
-        </div>
+        <>
+            <Navbar />
+            <div className='container mx-auto px-10 py-10'>
+                <div className='grid grid-cols-3 gap-4'>
+                <NoteCard title="Part Time Job" date="2025/09/03" content="Ramen Shop" tags="#17:00" isPinned={true}
+                onEdit={()=>{}} onDelete={()=>{}} OnPinNote={()=>{}}
+                />
+                </div>
+            </div> 
+            <button className='w-14 h-14 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-6 bottom-6'>
+                <MdAdd className='text-[32px] text-white'/>
+            </button>
+        </>
     )
 }
 
