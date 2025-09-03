9340e045aa22a0fec7b63c56af63455a42075afe
Hein Htet <littleshadow.mdy@gmail.com>
2025-09-03 09:48:03 +0900

    Added Search Bar

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileInfo from "../Cards/ProfileInfo"
 import { useNavigate } from 'react-router-dom'
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
     const navigate = useNavigate;
+    const [ querySearch, setQuerySearch] = useState("");
 
     const onLogout = () => {
         navigate('/login')
+    }
+
+    const handleSearch = () => {}
+    const clearSearch = () => {
+        setQuerySearch("")
     }
 
     return (
         <>
             <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
                 <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
-                
+                <SearchBar
+                value={querySearch}
+                    onChange={({ target }) => {
+                        setQuerySearch(target.value);
+                    }}
+                    handleSearch={handleSearch}
+                    clearSearch={clearSearch}
+                />
                 <ProfileInfo onLogout={ onLogout } />
             </div>
         </>
