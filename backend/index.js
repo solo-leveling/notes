b0d5b7d99343e82cb091408915938b004635bb62
Hein Htet <littleshadow.mdy@gmail.com>
2025-09-05 12:22:03 +0900

    Added new backend

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const connectToDB = require("./src/database/db");
+const homeRoutes = require("./src/routes/homeRoutes");
+const registerRoutes = require("./src/routes/registerRoutes");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//connect to database
+connectToDB();
+
+//route_test
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    data: "Route is working",
+  });
+});
+
+//routes
+app.use(registerRoutes);
+
+//get port from .env
+const PORT = process.env.PORT || 8001;
+
+//connection to server
+app.listen(PORT, () => {
+  console.log(`server is connected successfully at port ${PORT}`);
+});
