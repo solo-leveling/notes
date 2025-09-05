5bc453bc9a227c8c7b6443aa3d22c5e5dbf28528
Hein Htet <littleshadow.mdy@gmail.com>
2025-09-05 11:15:21 +0900

    Deleted backend

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerUser } = require('../controllers/userController');
-
-const router = express.Router();
-
-// Register new user
-router.post('/register', registerUser);
-
-module.exports = router;
