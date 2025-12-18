b3ec6c34904b3f50d6cbf60db17c25be81dc7126
Hein Htet <littleshadow.mdy@gmail.com>
2025-12-18 10:07:32 +0900

    Fixed code

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -7,16 +6,17 @@ const axiosInstance = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("token");
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
-
-(error) => {
-  return Promise.reject(error);
-};
+);
 
 export default axiosInstance;
