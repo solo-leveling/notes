5816929e39bb3c915495a31b66bd8ce27d8d0207
Hein Htet <littleshadow.mdy@gmail.com>
2025-12-18 11:03:13 +0900

    Ready to deploy

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,73 +1,91 @@
-import React, { useState } from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import { Link, useNavigate } from 'react-router-dom'
-import PasswordInput from '../../components/Inputs/PasswordInput'
-import { validateEmail } from '../../utils/helper'
-import axiosInstance from '../../utils/axiosInstance.js'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link, useNavigate } from "react-router-dom";
+import PasswordInput from "../../components/Inputs/PasswordInput";
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance.js";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState(null)
-    const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+    // if (email && !validateEmail(email)) {
+    //     setError("Please enter a valid email address.");
+    //     return;
+    // }
 
-        // if (email && !validateEmail(email)) {
-        //     setError("Please enter a valid email address.");
-        //     return;
-        // }
-
-        if (!password) {
-            setError("Please enter the password.");
-            return;
-        }
-
-        setError("")
-        
-        //Login in API call
-            try {
-                const response = await axiosInstance.post("/login", {
-                    email: email,
-                    username: email, // send the same value for both fields
-                    password: password
-                });
-
-            if (response.data && response.data.accessToken) {
-                localStorage.setItem("token", response.data.accessToken);
-                navigate("/dashboard")
-            }
-        } catch (error) {
-                if (error.response && error.response.data && error.response.data.message) {
-                    setError(error.response.data.message)
-                } else {
-                    setError("An unexpected error occurred. Please try again.")
-                }
-        }
+    if (!password) {
+      setError("Please enter the password.");
+      return;
     }
 
-    return <>
-        <Navbar />
-        <div className='flex items-center justify-center mt-28'>
-            <div className='w-96 border rounded bg-white px-7 py-10 shadow-xl'>
-                <form onSubmit={handleLogin}>
-                    <h4 className='flex items-center justify-center text-2xl mb-7'>Login</h4>
-                        <input type="text" placeholder='Email or Username' className='input-box'
-                    value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                    <PasswordInput value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                    {error && <p className='text-red-500 text-xs pb-1'>{ error }</p>}
-                    <button type='submit' className='btn-primary'>Login</button>
-                    <p className='text-sm text-center mt-4'>Not registered yet?{" "}
-                    <Link to="/signup" className='font-medium text-primary underline'>
-                        Create an Account        
-                    </Link></p>
-                </form>
-            </div>
-        </div>
-        </>
-    
-}
+    setError("");
 
-export default Login
+    //Login in API call
+    try {
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        username: email, // send the same value for both fields
+        password: password,
+      });
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/");
+      }
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10 shadow-xl">
+          <form onSubmit={handleLogin}>
+            <h4 className="flex items-center justify-center text-2xl mb-7">
+              Login
+            </h4>
+            <input
+              type="text"
+              placeholder="Email or Username"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+            <p className="text-sm text-center mt-4">
+              Not registered yet?{" "}
+              <Link to="/signup" className="font-medium text-primary underline">
+                Create an Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
